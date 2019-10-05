fdbd065799aefccee15cba39668287ddd72a87b9
Robert Wilson <rwilson@cfa/harvard.edu>
2019-10-05 15:46:53 +0000

    fix initial problems

--- dmyLO.py
+++ dmyLO.py
@@ -3,7 +3,7 @@ from redisPubSub import *
 
 # chan should be generated from the host name
 chan = "loa4"
-rps = RedisPubSub(channel = chan)
+rps = RedisPubSubGet(channel = chan)
 
 # There will be a lot of setting up the LO here
 
@@ -13,4 +13,5 @@ while True:
   print("received request from %s cmd=%s args=%s" % (origin, cmd, args))
   sleep(1)
   reply = "%s;%s;%s" % (origin, "ACK", cmd)
+  print("Sending reply:", reply)
   rps.respond(origin, reply)
--- redisPubSub.py
+++ redisPubSub.py
@@ -19,11 +19,12 @@ class RedisPubSubGet:
     self.redis.publish(self.channel+'->'+origin, message)
 
 class RedisPubSubSend:
-  def __init__(self, channel = 'test1', server="128.171.116.189"):
+  def __init__(self, channel, destination, server="128.171.116.189"):
     self.redis = StrictRedis(host=server, port = 6379)
     self.channel = channel
+    self.dest = destination
     self.ps = self.redis.pubsub()
-    self.ps.subscribe(channel)
+    self.ps.subscribe(channel+'->'+self.dest)
     
   def getResponse(self, timeout = 1000.):
     while True:
@@ -33,5 +34,6 @@ class RedisPubSubSend:
       if type(m) == dict and m['type'] == 'message':
         return(m['data'].decode("utf-8"))
 
-  def sendMessage(self, origin, message):
+  def sendCommand(self, cmd, args):
+    message = self.dest+';'+cmd+';'+args
     self.redis.publish(self.channel, message)
