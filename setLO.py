bcd393ce39b5e6bfed715493b2c930d6bbbfc583
Robert Wilson <rwilson@cfa.harvard.edu>
2019-10-10 16:06:32 -0400

    add readline, better cmd & arg processing

--- setLO.py
+++ setLO.py
@@ -1,13 +1,21 @@
+#! /usr/bin/python3
 from time import sleep
+import readline
 import os
 from redisPubSub import *
 
 # chan should be generated from the host name
-chan = "loa4"
+chan = "loa0"
 hostname = os.uname()[1]
 rps = RedisPubSubSend(channel = chan, destination = chan)
 
 while True:
-  inp = input("Enter desired frequency")
-  rps.sendCommand('tune', inp)
+  inp = input("Enter command arg1,arg2.. >").split()
+  if len(inp) > 1:
+    args = inp[1]
+  else:
+    args = ""
+  if inp[0][0] == 'q':
+    exit()
+  rps.sendCommand(inp[0], args)
   print(rps.getResponse(10))
