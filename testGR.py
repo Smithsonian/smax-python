8f202e7f0132a742de169b55dba93923f4e63e36
Robert Wilson <rwilson@cfa.harvard.edu>
2019-11-21 21:51:59 +0000

    Simple test program

---
+++ testGR.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3 -i
+
+import numpy as np
+from getFromRedis import *
+
+gr = GetFromRedis()
---
+++ testSR.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3 -i
+
+import numpy as np
+from sendToRedis import *
+
+sr = SendToRedis()
