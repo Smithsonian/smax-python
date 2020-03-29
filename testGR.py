b1903fb577e8f334b7bd68653eee4ef54d3306c2
Robert Wilson <rwilson@cfa.harvard.edu>
2020-03-29 16:14:19 +0000

    derive hostName

--- testGR.py
+++ testGR.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3 -i
 
+import os
 import numpy as np
 from getFromRedis import *
-key = "ScanningSpectrometers:t"
+hostName = os.uname()[1]
+if hostName == 'localhost':
+  redisIP = "192.168.0.1"
+else:
+  redisIP = "128.171.116.189"
+key = "ScanningSpectrometers:%c" % hostName[-1]
 
-
-gr = GetFromRedis(server = '192.168.0.1')
+gr = GetFromRedis(server = redisIP)
