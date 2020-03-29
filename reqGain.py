9e6c371060189fdedc062c7c6de6cb31031264da
Robert Wilson <rwilson@cfa.harvard.edu>
2020-03-29 11:48:23 -0400

    Simple program to request an auto gain setting

---
+++ reqGain.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3
+from time import sleep
+import readline
+import os
+from redisPubSub import *
+
+# chan should be generated from the host name
+chan = "scanspec9:rxB:IF0"
+rps = RedisPubSubSend(channel = chan, destination = chan)
+
+while True:
+  inp = input("Enter command arg1,arg2.. >").split()
+  if len(inp) > 1:
+    args = inp[1]
+  else:
+    args = ""
+  if inp[0][0] == 'q':
+    exit()
+  rps.sendCommand(inp[0], args)
+  print(rps.getResponse(10))
