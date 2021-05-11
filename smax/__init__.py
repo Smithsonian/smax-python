f421ffbad49e2322663845b1f677477d61bbda08
Christopher Moriarty <cmoriarty@gmail.com>
2021-05-11 12:28:49 -0400

    Added new SmaxClient into the __init__ import statements.

--- smax/__init__.py
+++ smax/__init__.py
@@ -2,3 +2,4 @@ from .sendToRedis import SendToRedis
 from .getFromRedis import GetFromRedis
 from .redisPubSub import RedisPubSubGet
 from .redisPubSub import RedisPubSubSend
+from .smax_client import SmaxClient
