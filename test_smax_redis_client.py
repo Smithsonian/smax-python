9beff65320d18ec69bc74e408e6169650c8330ae
Christopher Moriarty <cmoriarty@cfa.harvard.edu>
2021-07-07 12:34:30 -0400

    Updated all tests to use their test name as the root smax table to avoid any conflicts in tests.

--- test_smax_redis_client.py
+++ test_smax_redis_client.py
@@ -17,8 +17,8 @@ def test_context_manager():
     expected_data = "just a context manager string"
     expected_type = str
     expected_dim = 1
-    table = "pytest"
-    key = "test_context_manager_string"
+    table = "test_context_manager"
+    key = "pytest"
     with SmaxRedisClient("localhost") as s:
         s.smax_share(table, key, expected_data)
         result = s.smax_pull(table, key)
@@ -32,8 +32,8 @@ def test_roundtrip_string(smax_client):
     expected_data = "just a roundtrip string"
     expected_type = str
     expected_dim = 1
-    table = "pytest"
-    key = "test_roundtrip_string"
+    table = "test_roundtrip_string"
+    key = "pytest"
     smax_client.smax_share(table, key, expected_data)
     result = smax_client.smax_pull(table, key)
     assert result.data == expected_data
@@ -45,8 +45,8 @@ def test_roundtrip_int(smax_client):
     expected_data = 123456789
     expected_type = int
     expected_dim = 1
-    table = "pytest"
-    key = "test_roundtrip_int"
+    table = "test_roundtrip_int"
+    key = "pytest"
     smax_client.smax_share(table, key, expected_data)
     result = smax_client.smax_pull(table, key)
     assert result.data == expected_data
@@ -58,8 +58,8 @@ def test_roundtrip_string_list(smax_client):
     expected_data = ["i", "am", "list"]
     expected_type = str
     expected_dim = 3
-    table = "pytest"
-    key = "test_roundtrip_string_list"
+    table = "test_roundtrip_string_list"
+    key = "pytest"
     smax_client.smax_share(table, key, expected_data)
     result = smax_client.smax_pull(table, key)
     assert result.data == expected_data
@@ -72,8 +72,8 @@ def test_roundtrip_int_list(smax_client):
     expected_data = np.array(data)
     expected_type = type(data[0])
     expected_dim = len(data)
-    table = "pytest"
-    key = "test_roundtrip_string_list"
+    table = "test_roundtrip_int_list"
+    key = "pytest"
     smax_client.smax_share(table, key, data)
     result = smax_client.smax_pull(table, key)
     assert np.array_equal(result.data, expected_data.data)
@@ -86,8 +86,8 @@ def test_roundtrip_float_list(smax_client):
     expected_data = np.array(data)
     expected_type = expected_data.dtype
     expected_dim = len(data)
-    table = "pytest"
-    key = "test_roundtrip_float_list"
+    table = "test_roundtrip_float_list"
+    key = "pytest"
     smax_client.smax_share(table, key, expected_data)
     result = smax_client.smax_pull(table, key)
     assert np.array_equal(result.data, expected_data.data)
@@ -101,8 +101,8 @@ def test_roundtrip_2d_float_array(smax_client):
                               [-1.654321, -1.54321]])
     expected_type = expected_data.dtype
     expected_dim = expected_data.shape
-    table = "pytest"
-    key = "test_roundtrip_2d_float_array"
+    table = "test_roundtrip_2d_float_array"
+    key = "pytest"
     smax_client.smax_share(table, key, expected_data)
     result = smax_client.smax_pull(table, key)
     assert np.array_equal(result.data, expected_data.data)
@@ -111,12 +111,12 @@ def test_roundtrip_2d_float_array(smax_client):
 
 
 def test_pubsub(smax_client):
-    smax_client.smax_subscribe("pytest:test_pubsub")
     expected_data = "just a string"
     expected_type = str
     expected_dim = 1
-    table = "pytest"
-    key = "test_pubsub"
+    table = "test_pubsub"
+    key = "pytest"
+    smax_client.smax_subscribe(f"{table}:{key}")
     smax_client.smax_share(table, key, expected_data)
     result = smax_client.smax_wait_on_any_subscribed()
     assert result.data == expected_data
@@ -125,12 +125,12 @@ def test_pubsub(smax_client):
 
 
 def test_pubsub_pattern(smax_client):
-    smax_client.smax_subscribe("pytest:test_pubsub*")
     expected_data = "just a string"
     expected_type = str
     expected_dim = 1
-    table = "pytest:test_pubsub"
-    key = "pattern"
+    table = "test_pubsub_pattern"
+    key = "pytest"
+    smax_client.smax_subscribe(f"{table}:{key}*")
     smax_client.smax_share(table, key, expected_data)
     result = smax_client.smax_wait_on_any_subscribed()
     assert result.data == expected_data
@@ -139,12 +139,12 @@ def test_pubsub_pattern(smax_client):
 
 
 def test_pubsub_with_timeout(smax_client):
-    smax_client.smax_subscribe("pytest:test_pubsub_with_timeout")
     expected_data = "just a timeout string"
     expected_type = str
     expected_dim = 1
-    table = "pytest"
-    key = "test_pubsub_with_timeout"
+    table = "test_pubsub_with_timeout"
+    key = "pytest"
+    smax_client.smax_subscribe(f"{table}:{key}")
     smax_client.smax_share(table, key, expected_data)
     result = smax_client.smax_wait_on_any_subscribed(timeout=3.0)
     assert result.data == expected_data
@@ -153,17 +153,19 @@ def test_pubsub_with_timeout(smax_client):
 
 
 def test_pubsub_with_timeout_exception(smax_client):
-    smax_client.smax_subscribe("pytest:test_pubsub")
+    table = "test_pubsub_with_timeout_exception"
+    key = "pytest"
+    smax_client.smax_subscribe(f"{table}:{key}")
     with pytest.raises(TimeoutError):
         smax_client.smax_wait_on_any_subscribed(timeout=.5)
 
 
 def test_pubsub_notification(smax_client):
-    smax_client.smax_subscribe("pytest:test_pubsub")
+    table = "test_pubsub_notification"
+    key = "pytest"
     expected_data = socket.gethostname()
-    expected_channel = "smax:pytest:test_pubsub"
-    table = "pytest"
-    key = "test_pubsub"
+    expected_channel = f"smax:{table}:{key}"
+    smax_client.smax_subscribe(f"{table}:{key}")
     smax_client.smax_share(table, key, "doesn't matter")
     result = smax_client.smax_wait_on_any_subscribed(notification_only=True)
     assert result["data"].decode("utf-8") == expected_data
@@ -171,29 +173,33 @@ def test_pubsub_notification(smax_client):
 
 
 def test_pubsub_wait_on_pattern(smax_client):
-    smax_client.smax_subscribe("pytest:test_pubsub*")
+    table = "test_pubsub_wait_on_pattern"
+    key = "pytest"
+    smax_client.smax_subscribe(f"{table}:{key}*")
     expected_data1 = 666
     expected_data2 = 33
-    smax_client.smax_share("pytest:test_pubsub:nop", "nop", "nopvalue")
-    smax_client.smax_share("pytest:test_pubsub:fpga", "temp", expected_data1)
-    smax_client.smax_share("pytest:test_pubsub:fpga", "speed", expected_data2)
-    result1 = smax_client.smax_wait_on_subscribed("pytest:test_pubsub:fpga*")
-    result2 = smax_client.smax_wait_on_subscribed("pytest:test_pubsub:fpga*")
-    assert result1["pytest"]["test_pubsub"]["fpga"]["temp"].data == expected_data1
-    assert result2["pytest"]["test_pubsub"]["fpga"]["speed"].data == expected_data2
+    smax_client.smax_share(f"{table}:{key}:nop", "nop", "nopvalue")
+    smax_client.smax_share(f"{table}:{key}:fpga", "temp", expected_data1)
+    smax_client.smax_share(f"{table}:{key}:fpga", "speed", expected_data2)
+    result1 = smax_client.smax_wait_on_subscribed(f"{table}:{key}:fpga*")
+    result2 = smax_client.smax_wait_on_subscribed(f"{table}:{key}:fpga*")
+    assert result1[table][key]["fpga"]["temp"].data == expected_data1
+    assert result2[table][key]["fpga"]["speed"].data == expected_data2
 
 
 def test_pubsub_pattern_callback(smax_client):
+    table = "test_pubsub_pattern_callback"
+    key = "pytest"
     expected_value = 42
 
     # Inner functions can't modify outer variables unless they are mutable.
     actual = {"value": None}
 
     def my_callback(message):
-        actual["value"] = message["pytest"]["pubsub_callback"]["fpga1"]["temp"].data
+        actual["value"] = message[table][key]["fpga1"]["temp"].data
 
-    smax_client.smax_subscribe("pytest:pubsub_callback*", my_callback)
-    smax_client.smax_share("pytest:pubsub_callback:fpga1", "temp", expected_value)
+    smax_client.smax_subscribe(f"{table}:{key}*", callback=my_callback)
+    smax_client.smax_share(f"{table}:{key}:fpga1", "temp", expected_value)
 
     # Sleep and then check actual value
     sleep(.1)
@@ -210,8 +216,10 @@ def test_pubsub_callback(smax_client):
     def my_callback(message):
         actual["value"] = message.data
 
-    smax_client.smax_subscribe("pytest:callback:fpga1:temp", my_callback)
-    smax_client.smax_share("pytest:callback:fpga1", "temp", expected_value)
+    table = "test_pubsub_callback"
+    key = "pytest"
+    smax_client.smax_subscribe(f"{table}:{key}:fpga1:temp", callback=my_callback)
+    smax_client.smax_share(f"{table}:{key}:fpga1", "temp", expected_value)
 
     # Sleep and then check actual value
     sleep(.1)
@@ -232,13 +240,16 @@ def test_multiple_pubsub_callback(smax_client):
     def my_callback2(message):
         actual["value2"] = message.data
 
-    smax_client.smax_subscribe("pytest:callback:fpga1:temp", my_callback1)
-    smax_client.smax_subscribe("pytest:callback:fpga2:temp", my_callback2)
-    smax_client.smax_share("pytest:callback:fpga1", "temp", expected_value1)
-    smax_client.smax_share("pytest:callback:fpga2", "temp", expected_value2)
+    table = "test_multiple_pubsub_callback"
+    key = "pytest"
+    smax_client.smax_subscribe(f"{table}:{key}:fpga1:temp", callback=my_callback1)
+    smax_client.smax_subscribe(f"{table}:{key}:fpga2:temp", callback=my_callback2)
+
+    smax_client.smax_share(f"{table}:{key}:fpga1", "temp", expected_value1)
+    smax_client.smax_share(f"{table}:{key}:fpga2", "temp", expected_value2)
 
     # Sleep and then check actual value
-    sleep(.5)
+    sleep(1)
     assert actual["value1"] == expected_value1
     assert actual["value2"] == expected_value2
 
@@ -253,16 +264,18 @@ def test_pull_struct(smax_client):
     expected_type_firmware = float
     expected_dim_firmware = 1
 
-    smax_client.smax_share("swarm:dbe:roach2-01", "temp", expected_temp_value1)
-    smax_client.smax_share("swarm:dbe:roach2-02", "temp", expected_temp_value2)
-    smax_client.smax_share("swarm:dbe:roach2-01", "firmware", expected_firmware_value1)
-    smax_client.smax_share("swarm:dbe:roach2-02", "firmware", expected_firmware_value2)
-    result = smax_client.smax_pull("swarm", "dbe")
+    table = "test_pull_struct"
 
-    roach01_temp = result["swarm"]["dbe"]["roach2-01"]["temp"]
-    roach02_temp = result["swarm"]["dbe"]["roach2-02"]["temp"]
-    roach01_firmware = result["swarm"]["dbe"]["roach2-01"]["firmware"]
-    roach02_firmware = result["swarm"]["dbe"]["roach2-02"]["firmware"]
+    smax_client.smax_share(f"{table}:swarm:dbe:roach2-01", "temp", expected_temp_value1)
+    smax_client.smax_share(f"{table}:swarm:dbe:roach2-02", "temp", expected_temp_value2)
+    smax_client.smax_share(f"{table}:swarm:dbe:roach2-01", "firmware", expected_firmware_value1)
+    smax_client.smax_share(f"{table}:swarm:dbe:roach2-02", "firmware", expected_firmware_value2)
+    result = smax_client.smax_pull(f"{table}:swarm", "dbe")
+
+    roach01_temp = result[table]["swarm"]["dbe"]["roach2-01"]["temp"]
+    roach02_temp = result[table]["swarm"]["dbe"]["roach2-02"]["temp"]
+    roach01_firmware = result[table]["swarm"]["dbe"]["roach2-01"]["firmware"]
+    roach02_firmware = result[table]["swarm"]["dbe"]["roach2-02"]["firmware"]
 
     assert (roach01_temp.data == expected_temp_value1).all()
     assert (roach02_temp.data == expected_temp_value2).all()
@@ -279,6 +292,7 @@ def test_pull_struct(smax_client):
 
 
 def test_share_struct(smax_client):
+    table = "test_share_struct"
     expected_temp_value1 = 100
     expected_temp_value2 = 0
     expected_firmware_value1 = 2.0
@@ -291,12 +305,12 @@ def test_share_struct(smax_client):
     struct = {"roach2-03": {"temp": expected_temp_value1, "firmware": expected_firmware_value1},
               "roach2-04": {"temp": expected_temp_value2, "firmware": expected_firmware_value2}}
 
-    smax_client.smax_share("swarm", "dbe", struct)
-    result = smax_client.smax_pull("swarm", "dbe")
-    roach03_temp = result["swarm"]["dbe"]["roach2-03"]["temp"]
-    roach04_temp = result["swarm"]["dbe"]["roach2-04"]["temp"]
-    roach03_firmware = result["swarm"]["dbe"]["roach2-03"]["firmware"]
-    roach04_firmware = result["swarm"]["dbe"]["roach2-04"]["firmware"]
+    smax_client.smax_share(f"{table}:swarm", "dbe", struct)
+    result = smax_client.smax_pull(f"{table}:swarm", "dbe")
+    roach03_temp = result[table]["swarm"]["dbe"]["roach2-03"]["temp"]
+    roach04_temp = result[table]["swarm"]["dbe"]["roach2-04"]["temp"]
+    roach03_firmware = result[table]["swarm"]["dbe"]["roach2-03"]["firmware"]
+    roach04_firmware = result[table]["swarm"]["dbe"]["roach2-04"]["firmware"]
 
     # Data and type checks.
     assert roach03_temp.data == expected_temp_value1
@@ -315,38 +329,46 @@ def test_share_struct(smax_client):
 
 def test_roundtrip_meta(smax_client):
     # Do a normal share to generate the automatic metadata.
-    smax_client.smax_share("pytest", "test_roundtrip_meta", "Doesn't Matter")
+    table = "test_roundtrip_meta"
+    key = "pytest"
+    smax_client.smax_share(table, key, "Doesn't Matter")
 
     # Now change metadata for timestamps to equal "1".
     expected_value = "1"
-    smax_client.smax_push_meta("timestamps", "pytest:test_roundtrip_meta", expected_value)
+    smax_client.smax_push_meta("timestamps", f"{table}:{key}", expected_value)
 
     # Now pull just metadata.
-    result = smax_client.smax_pull_meta("pytest:test_roundtrip_meta", "timestamps")
+    result = smax_client.smax_pull_meta(f"{table}:{key}", "timestamps")
     assert result == expected_value
 
 
 def test_description_meta(smax_client):
+    table = "test_description_meta"
+    key = "pytest"
+
     # Do a normal share to generate the automatic metadata.
-    smax_client.smax_share("pytest", "test_units_meta", "Doesn't Matter")
+    smax_client.smax_share(table, key, "Doesn't Matter")
 
     # Now add units metadata.
     expected_value = "I am a description"
-    smax_client.smax_set_description("pytest:test_description_meta", expected_value)
+    smax_client.smax_set_description(f"{table}:{key}", expected_value)
 
     # Now pull just metadata.
-    result = smax_client.smax_get_description("pytest:test_description_meta")
+    result = smax_client.smax_get_description(f"{table}:{key}")
     assert result == expected_value
 
 
 def test_units_meta(smax_client):
+    table = "test_units_meta"
+    key = "pytest"
+
     # Do a normal share to generate the automatic metadata.
-    smax_client.smax_share("pytest", "test_units_meta", "Doesn't Matter")
+    smax_client.smax_share(table, key, "Doesn't Matter")
 
     # Now add units metadata.
     expected_value = "feet"
-    smax_client.smax_set_units("pytest:test_units_meta", expected_value)
+    smax_client.smax_set_units(f"{table}:{key}", expected_value)
 
     # Now pull just metadata.
-    result = smax_client.smax_get_units("pytest:test_units_meta")
+    result = smax_client.smax_get_units(f"{table}:{key}")
     assert result == expected_value
