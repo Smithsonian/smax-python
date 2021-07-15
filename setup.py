b0e80541dcccd3e2eeb202ae3d3e58c13bec893c
Christopher Moriarty <cmoriarty@cfa.harvard.edu>
2021-07-15 11:04:55 -0400

    Specified a version of redis api, older versions seem incompatible.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import setuptools
 setuptools.setup(
     name="smax",
     version="1.0.1",
-    install_requires=['numpy', 'pytest', 'redis', 'hiredis'],
+    install_requires=['numpy', 'pytest', 'redis>=3.5.3', 'hiredis'],
     packages=setuptools.find_packages()
 )
