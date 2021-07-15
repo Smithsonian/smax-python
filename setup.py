55efdf83780d331b54c29d362bde92bbe8916ecc
Christopher Moriarty <cmoriarty@cfa.harvard.edu>
2021-07-15 10:45:14 -0400

    Ticked the version up for the python 3.7 fix.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="smax",
-    version="1.0",
+    version="1.0.1",
     install_requires=['numpy', 'pytest', 'redis', 'hiredis'],
     packages=setuptools.find_packages()
 )
