1f57d00662fe54a6631a429fc36f57aeb11f59a4
Paul Grimes <pgrimes@cfa.harvard.edu>
2022-02-14 12:03:47 -0500

    Bump version before merging

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="smax",
-    version="1.0.2",
+    version="1.0.3",
     install_requires=['numpy', 'pytest', 'redis>=3.5.3', 'hiredis'],
     packages=setuptools.find_packages()
 )
