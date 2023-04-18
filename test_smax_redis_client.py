0cce6c21538b15baaa1f7f17c63b8123c0374633
Paul Grimes <pgrimes@cfa.harvard.edu>
2023-04-18 15:02:23 -0400

    Try a different debug

--- test_smax_redis_client.py
+++ test_smax_redis_client.py
@@ -41,9 +41,9 @@ def test_redis_HGetWithMeta():
     hget_sha = ps.stdout.decode().strip()
     ps = subprocess.run(f"redis-cli -h {smax_redis_ip} EVALSHA {hget_sha} 1 scripts HGetWithMeta".split(" "), capture_output=True)
     logger.debug(ps)
-    
-    ps = subprocess.run(f"redis-cli -h {smax_redis_ip} HGETALL *".split(" "), capture_output=True)
-    logger.debug(ps)
+    ps = subprocess.run(f"redis-cli -h {smax_redis_ip} KEYS *".split(" "), capture_output=True)
+    keys = ps.stdout.split(b'\n')
+    logger.debug(keys)
     assert ps.stdout.decode().strip() == hget_sha
     
     
