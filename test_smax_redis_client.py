15b52a0f4aca4115265b40228cfb72e40e199155
Paul Grimes <pgrimes@cfa.harvard.edu>
2023-04-19 09:13:40 -0400

    Got correct checkout, now fix debugging code

--- test_smax_redis_client.py
+++ test_smax_redis_client.py
@@ -39,7 +39,7 @@ def test_redis_scripts():
 def test_redis_HGetWithMeta():
     ps = subprocess.run(f"redis-cli -h {smax_redis_ip} HGET scripts HGetWithMeta".split(" "), capture_output=True)
     hget_sha = ps.stdout.decode().strip()
-    ts = subprocess.run(f"redis-cli -h {smax_redis_ip} HGETAll scripts".split(" "), capture_output=True)
+    ts = subprocess.run(f"redis-cli -h {smax_redis_ip} HGET scripts HSET".split(" "), capture_output=True)
     hset_sha = ts.stdout.decode().strip()
     logger.debug(hget_sha)
     logger.debug(hset_sha)
