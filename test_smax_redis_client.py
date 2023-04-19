b24907569d7aaf684ec59fb60d1077891f55698c
Paul Grimes <pgrimes@cfa.harvard.edu>
2023-04-19 08:55:29 -0400

    try getting all keys from scripts

--- test_smax_redis_client.py
+++ test_smax_redis_client.py
@@ -39,7 +39,7 @@ def test_redis_scripts():
 def test_redis_HGetWithMeta():
     ps = subprocess.run(f"redis-cli -h {smax_redis_ip} HGET scripts HGetWithMeta".split(" "), capture_output=True)
     hget_sha = ps.stdout.decode().strip()
-    ps = subprocess.run(f"redis-cli -h {smax_redis_ip} HGET scripts HSet".split(" "), capture_output=True)
+    ps = subprocess.run(f"redis-cli -h {smax_redis_ip} HGET scripts *".split(" "), capture_output=True)
     hset_sha = ps.stdout.decode().strip()
     logger.debug(hget_sha)
     logger.debug(hset_sha)
