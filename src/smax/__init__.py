4cea38829911dd6db5f397bbcf6852256dd556ee
Paul Grimes <pgrimes@cfa.harvard.edu>
2024-08-09 15:49:05 -0400

    Add version tag to __init__.py

--- src/smax/__init__.py
+++ src/smax/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "1.0.0"
+
 from .smax_client import SmaxData, SmaxInt, SmaxFloat, SmaxBool, SmaxStr, \
     SmaxStrArray, SmaxArray, SmaxStruct, SmaxInt8, SmaxInt16, SmaxInt32, \
     SmaxInt64, SmaxFloat32, SmaxFloat64, SmaxBool, \
