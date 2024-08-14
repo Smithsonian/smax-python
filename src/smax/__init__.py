878192c299356af195ac8a871e1bf18e33e9838b
Paul Grimes <pgrimes@cfa.harvard.edu>
2024-08-14 14:29:13 -0400

    Bump version to 1.0.2 incorporating CLI fix

--- src/smax/__init__.py
+++ src/smax/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 from .smax_client import SmaxData, SmaxInt, SmaxFloat, SmaxBool, SmaxStr, \
     SmaxStrArray, SmaxArray, SmaxStruct, SmaxInt8, SmaxInt16, SmaxInt32, \
