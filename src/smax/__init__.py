75585e7c72fac4e6c6109ac0280c4468eee92703
Paul Grimes <pgrimes@cfa.harvard.edu>
2025-03-20 15:57:21 -0400

    bump to 1.0.5 to properly fix the issue.

--- src/smax/__init__.py
+++ src/smax/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 from .smax_client import SmaxData, SmaxInt, SmaxFloat, SmaxBool, SmaxStr, \
     SmaxStrArray, SmaxArray, SmaxStruct, SmaxInt8, SmaxInt16, SmaxInt32, \
