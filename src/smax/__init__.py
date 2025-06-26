8bb160c1ce5ce2342cca8a14d588ea35540dad74
Paul Grimes <pgrimes@cfa.harvard.edu>
2025-06-26 16:33:42 -0400

    Bump to 1.2.1

--- src/smax/__init__.py
+++ src/smax/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 from .smax_client import SmaxData, SmaxInt, SmaxFloat, SmaxBool, SmaxStr, \
     SmaxStrArray, SmaxArray, SmaxStruct, SmaxInt8, SmaxInt16, SmaxInt32, \
