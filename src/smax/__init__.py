9a1dd6b6d6d3ec32861363d4426af88bcf6641c3
Paul Grimes <pgrimes@cfa.harvard.edu>
2025-08-28 10:40:21 -0400

    Patch for default logging level and to remove old code

--- src/smax/__init__.py
+++ src/smax/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.2.3'
+__version__ = '1.2.4'
 
 from .smax_client import SmaxData, SmaxInt, SmaxFloat, SmaxBool, SmaxStr, \
     SmaxStrArray, SmaxArray, SmaxStruct, SmaxInt8, SmaxInt16, SmaxInt32, \
